3afb4d06244d0aea3c834db1bb03caf6775096b5
Kouta Takamatsu <bb1016181@gmail.com>
2019-02-04 15:46:39 +0900

    favButtomの実装

--- app/src/main/java/kouta/submit/ListAdapter.java
+++ app/src/main/java/kouta/submit/ListAdapter.java
@@ -1,27 +1,36 @@
 package kouta.submit;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ListAdapter extends BaseAdapter {
 
     private String[] list={"りんご","みかん","いちご","なし","ぶどう","メロン","スイカ","さくらんぼ","グレープフルーツ","もも","バナナ"};
-    private LayoutInflater mLayoutInflater;
-    private Context mContext;
+    private ListFragment listFragment;
+
+    public ListAdapter(ListFragment listFragment) {
+        super();
+        this.listFragment = listFragment;
+    }
 
     private static class ViewHolder {
         public TextView dateText;
-    }
-
-    public ListAdapter(Context context){
-        mContext=context;
-        mLayoutInflater= LayoutInflater.from(context);
     }
 
     @Override
@@ -31,12 +40,12 @@ public class ListAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
@@ -44,6 +53,7 @@ public class ListAdapter extends BaseAdapter {
 
         ViewHolder holder;
         if (convertView == null) {
+        LayoutInflater mLayoutInflater = listFragment.getLayoutInflater();
             convertView = mLayoutInflater.inflate(R.layout.list_item, null);
             holder = new ViewHolder();
             holder.dateText = convertView.findViewById(R.id.item_text);
@@ -51,6 +61,61 @@ public class ListAdapter extends BaseAdapter {
         } else {
             holder = (ViewHolder)convertView.getTag();
         }
+
+        //↓お気に入りボタンの実装
+
+        final ImageButton favButton =convertView.findViewById(R.id.favButton);
+
+        final SharedPreferences pre = PreferenceManager.getDefaultSharedPreferences(convertView.getContext());
+        final Gson gson = new Gson();
+        final String jsonText = pre.getString("favoriteList", "[]");
+        Log.d("checkjson",jsonText);
+        ArrayList arrayList = gson.fromJson(jsonText, new TypeToken<ArrayList<Integer>>(){}.getType());
+        final int id = Integer.parseInt(String.valueOf(position));
+
+        if (arrayList.indexOf(id) != -1){
+            favButton.setImageResource(R.drawable.ic_favorite_black_48dp);
+        }
+
+        final View finalConvertView = convertView;
+        favButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String jsonTextButton = pre.getString("favoriteList", "[]");
+                final ArrayList favoriteList = gson.fromJson(jsonTextButton, new TypeToken<ArrayList<Integer>>(){}.getType());
+                Log.d("checkfavorite", String.valueOf(favoriteList));
+                Animation animation = AnimationUtils.loadAnimation(finalConvertView.getContext(), R.anim.touch);
+                animation.setAnimationListener(new Animation.AnimationListener() {
+
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        String text;
+                        if (favoriteList.indexOf(id) == -1) {
+                            favButton.setImageResource(R.drawable.ic_favorite_black_48dp);
+                            favoriteList.add(id);
+                            text = gson.toJson(favoriteList);
+                            pre.edit().putString("favoriteList",text).apply();
+                        }else if (favoriteList.indexOf(id) != -1) {
+                            favButton.setImageResource(R.drawable.ic_favorite_border_black_48dp);
+                            favoriteList.removeAll(Collections.singleton(id));
+                            text = gson.toJson(favoriteList);
+                            pre.edit().putString("favoriteList",text).apply();
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                v.startAnimation(animation);
+            }
+        });
+        //↑
 
         holder.dateText.setText(list[position]);
 
