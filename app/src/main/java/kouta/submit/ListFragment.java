cb9d70a96e6aa7795569a7d428cb03a7315c54af
Kouta Takamatsu <bb1016181@gmail.com>
2019-02-04 11:11:48 +0900

    tablayoutとviewpagerの追加

---
+++ app/src/main/java/kouta/submit/ListFragment.java
@@ -0,0 +1,28 @@
+package kouta.submit;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_list, container, false);
+        super.onCreate(savedInstanceState);
+
+        return layout;
+
+    }
+}
